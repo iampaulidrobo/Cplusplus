94803283aed65ecdd4812a8691a7a03d4bb82c83
Piyush Tailor <piyushtailor4@gmail.com>
2024-08-08 08:31:22 +0530

    repo restructured

---
+++ IX.stacksandqueue/is_usingqueue.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Stack {
+	// Two inbuilt queues
+	queue<int> q1, q2;
+
+public:
+	void push(int x)
+	{
+		// Push x first in empty q2
+		q2.push(x);
+
+		// Push all the remaining
+		// elements in q1 to q2.
+		while (!q1.empty()) {
+			q2.push(q1.front());
+			q1.pop();
+		}
+
+		// swap the names of two queues
+		queue<int> q = q1;
+		q1 = q2;
+		q2 = q;
+	}
+
+	void pop()
+	{
+		// if no elements are there in q1
+		if (q1.empty())
+			return;
+		q1.pop();
+	}
+
+	int top()
+	{
+		if (q1.empty())
+			return -1;
+		return q1.front();
+	}
+
+	int size() { return q1.size(); }
+};
+
+// Driver code
+int main()
+{
+	Stack s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+
+	cout << "current size: " << s.size() << endl;
+	cout << s.top() << endl;
+	s.pop();
+	cout << s.top() << endl;
+	s.pop();
+	cout << s.top() << endl;
+
+	cout << "current size: " << s.size() << endl;
+	return 0;
+}
+
+
+class Stack {
+  queue < int > q;
+  public:
+    void Push(int x) {
+      int s = q.size();
+      q.push(x);
+      for (int i = 0; i < s; i++) {
+
+        q.push(q.front());
+        q.pop();
+      }
+    }
+  int Pop() {
+    int n = q.front();
+    q.pop();
+    return n;
+  }
+  int Top() {
+    return q.front();
+  }
+  int Size() {
+    return q.size();
+  }
+};
--- XI.LinkedList/Theory.md
+++
@@ -1,2 +0,0 @@
-Concluded Theory:
-Linked List:
